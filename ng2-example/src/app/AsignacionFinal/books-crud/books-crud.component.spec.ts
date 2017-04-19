37cf320509493b4ade0abf4b745a6cd384a51953
pierre dell pc <pierre.alni@gmail.com>
2017-04-18 22:42:57 -0500

    Books Crud Component unit test finished

--- ng2-example/src/app/AsignacionFinal/books-crud/books-crud.component.spec.ts
+++ ng2-example/src/app/AsignacionFinal/books-crud/books-crud.component.spec.ts
@@ -5,18 +5,20 @@ import 'rxjs/Rx';
 
 import { BooksCrudComponent } from './books-crud.component';
 import { BooksCrudService } from './shared/books-crud.service';
-//import { BooksCrud } from './shared/books-crud.model';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
-describe('a books-crud component', () => {
+fdescribe('A Books Crud Component', () => {
 	let component: BooksCrudComponent;
-
 	// register all needed dependencies
 	beforeEach(() => {
 		TestBed.configureTestingModule({
 			imports: [HttpModule],
+			schemas: [CUSTOM_ELEMENTS_SCHEMA],
 			providers: [
 				{ provide: BooksCrudService, useClass: MockBooksCrudService },
-				BooksCrudComponent
+				BooksCrudComponent,
+				FormBuilder
 			]
 		});
 	});
@@ -26,14 +28,34 @@ describe('a books-crud component', () => {
 		component = BooksCrudComponent;
 	}));
 
-	it('should have an instance', () => {
+	it('should have an instance ', () => {
 		expect(component).toBeDefined();
+	});
+
+	it('should have an undefined action', () => {
+		expect(component.action).toBe(undefined);
+	})
+
+	it('should fecth list on ngInit', () => {
+		//Arrange
+		let model: any = {
+			Data: [{}], TotalRows: 1, TotalPages: 1
+		};
+		let service = TestBed.get(BooksCrudService);
+		service.getBooks = () => {
+			return Observable.of(model)
+		}
+		//Act
+		component.ngOnInit();
+		//Assert
+		expect(component.books).toBe(model);
+		expect(component.books.length).toBe(model.length);
 	});
 });
 
-// Mock of the original books-crud service
+// Mock of the original books service
 class MockBooksCrudService extends BooksCrudService {
-	getList(): Observable<any> {
-		return Observable.from([ { id: 1, name: 'One'}, { id: 2, name: 'Two'} ]);
+	getBooks(): Observable<any> {
+		return Observable.from([{ id: 1, name: 'One' }, { id: 2, name: 'Two' }]);
 	}
 }
